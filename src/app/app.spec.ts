76d4cfefa90cada0ec7202381f1deffa964aaef9
Ella Conley <egconley@bu.edu>
2025-06-19 21:49:14 -0500

    updated unit test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,8 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'welcome to tamiffgotchi',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('tamiffgotchi');
   });
 });
